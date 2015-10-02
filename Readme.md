b74b1c92ae54b2a6b76d9f6edbd936c990c888c0
davidmarkclements <huperekchuno@googlemail.com>
2015-10-02 14:45:23 +0100

    1st

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# React Number Control
+
+Bare bones number control - functionality only, you supply the styling.
+
+## Usage
+
+```js
+const React = require('react')
+const NumberSpinner = require('react-number-control')
+ 
+const spinner = <NumberSpinner 
+  max={4} 
+  min={0} 
+  n={0} 
+  className='user-supplied-main-class'
+  childClasses={
+    {
+      minus: 'user-supplied-minus-class',
+      num: 'user-supplied-num-class',
+      plus: 'user-supplied-plus-class'
+    }
+  }
+  minusChar='-'
+  plusChar='+'
+/>
+
+```
+
+All arguments are optional. 
+
+
+
+## Credits
+
+Sponsored by [nearForm](http://nearform.com)
+
+
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const React = require('react')
+const {Component} = React
+
+class NumberSpinner extends Component {
+  constructor({max = 4, min = 0, n = 0}) {
+    super()
+    this.setState({max, min, n})
+  }
+  compenentWillReceiveProps({max = 4, min = 0, n = 0}) {
+    this.setState({max, min, n})
+  }
+  minus() {
+    const {min} = this.state
+    let {n} = this.state
+    n -= 1
+    console.log(n, min)
+    if (n >= min) this.setState({n: n})
+  }
+  plus() {
+    const {max} = this.state
+    let {n} = this.state
+    n += 1
+    console.log(n, max)
+    if (n <= max) this.setState({n: n})
+  }
+  render() {
+    const {n} = this.state
+    const {className = 'ns', childClasses = {}, chars = {}} = this.props
+    const {minus = 'ns-minus', num = 'ns-num', plus = 'ns-plus'} = childClasses
+    const {minusChar = '-', plusChar = '+'} = chars
+    return (
+      <div className={className}>
+        <span onClick={() => this.minus()} className={minus} key='ns-minus'>
+          {minusChar}
+        </span>
+        <span className={plus} key='ns-num'>{n}</span>
+        <span onClick={() => this.plus()} className={num} key='ns-plus'>
+          {plusChar}
+        </span>
+      </div>
+    )
+
+  }
+}
+
+module.exports = NumberSpinner
